db2a8687ae749fbbb1ee4af43289365547186afa
Jose David Baena <josedab@gmail.com>
2014-12-31 16:58:26 +0000

    NT Initial setup for maven project

---
+++ vimeo-api-client/src/main/java/org/technobitia/vimeo_api_client/App.java
@@ -0,0 +1,13 @@
+package org.technobitia.vimeo_api_client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
