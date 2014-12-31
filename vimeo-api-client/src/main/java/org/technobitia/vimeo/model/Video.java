cc4ed1aa5225b00fc1ef16c622d55663feb471d7
Jose David Baena <josedab@gmail.com>
2014-12-31 17:39:04 +0000

    NT Reestructure Vimeo video model

--- vimeo-api-client/src/main/java/org/technobitia/vimeo/model/Video.java
+++ vimeo-api-client/src/main/java/org/technobitia/vimeo/model/Video.java
@@ -1,18 +1,151 @@
 package org.technobitia.vimeo.model;
 
 import java.util.Date;
+import java.util.List;
+
+import com.google.common.collect.Lists;
 
 public class Video {
+    
+    private String name;
+    private String description;
+    private Date publishedAt;
+    private User user;
     private String status;
     private String link;
-    private int viewCount;
     private String uri;
     private int duration;
     private int width;
     private int height;
-    private String name;
-    private String description;
     private String contentRating;
-    private Date publishedAt;
-    private User user;
+    private int viewCount;
+    private int likeCount;
+    private int commentCount;
+
+    private List<Tag> tags;
+
+    public Video() {
+        tags = Lists.newArrayList();
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContentRating() {
+        return contentRating;
+    }
+
+    public void setContentRating(String contentRating) {
+        this.contentRating = contentRating;
+    }
+
+    public Date getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(Date publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(int viewCount) {
+        this.viewCount = viewCount;
+    }
+
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(int likeCount) {
+        this.likeCount = likeCount;
+    }
+
+    public int getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(int commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+
 }
