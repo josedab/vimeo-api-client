0467c2f9f9b05cd23725075cdba8d562b86f605a
Jose David Baena <josedab@gmail.com>
2014-12-31 17:38:08 +0000

    NT Updated User model with getters and setters

--- vimeo-api-client/src/main/java/org/technobitia/vimeo/model/User.java
+++ vimeo-api-client/src/main/java/org/technobitia/vimeo/model/User.java
@@ -1,9 +1,67 @@
 package org.technobitia.vimeo.model;
 
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
 public class User {
     private String bio;
     private String location;
     private String link;
     private String name;
-    private 
+    private String pictureUrl;
+    private List<Website> websites;
+
+    public User() {
+        websites = Lists.newArrayList();
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    public List<Website> getWebsites() {
+        return websites;
+    }
+
+    public void setWebsites(List<Website> websites) {
+        this.websites = websites;
+    }
+
 }
