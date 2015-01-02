1743fd9c6a2d69b7cafb447efad1dbffdfddf30f
Jose David Baena <josedab@gmail.com>
2015-01-02 18:59:50 +0000

    IS-4 Website extractor development - Closes #4

--- vimeo-api-client/src/main/java/org/technobitia/vimeo/extractors/WebsiteExtractor.java
+++ vimeo-api-client/src/main/java/org/technobitia/vimeo/extractors/WebsiteExtractor.java
@@ -7,8 +7,11 @@ import com.google.common.base.Function;
 
 public class WebsiteExtractor implements Function<JSONObject, Website> {
 
-    public Website apply(JSONObject arg0) {
+    public Website apply(JSONObject jsonData) {
         Website website = new Website();
+        website.setLink(jsonData.getString("link"));
+        website.setName(jsonData.getString("name"));
+        website.setDescription(jsonData.getString("description"));
         return website;
     }
 
